326860ecb549701eca1e82fa002068dbaeb325d4
Tianying Xia <xiaty1@gmail.com>
2021-10-06 06:21:18 -0600

    Deploy

--- publishconf.py
+++ publishconf.py
@@ -17,6 +17,7 @@ FEED_ALL_ATOM = 'feeds/all.atom.xml'
 #CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
+#OUTPUT_PATH = "docs/"
 
 # Following items are often useful when publishing
 
